a52f6102fbb331448417049ae9cc3281ae7f9380
Vitor Brito <vitorlsbrito@gmail.com>
2025-10-20 02:45:52 +0000

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop:{" "}
+          <span className="text-blue">Find an Apple Store</span> or{" "}
+          <span className="text-blue">other retailer near you</span>. Or call
+          00800 040 1966.
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright ⓒ 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   );
 };
 
